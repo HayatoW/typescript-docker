02ef42525d25a8a0f49c2c1a7c704cdb1b09b606
Hayato Watanabe <hayato.watanabe@serverworks.co.jp>
2023-12-29 23:01:49 +0900

    paste tutorial code

--- main.ts
+++ main.ts
@@ -1,26 +1,25 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
-import { DockerProvider } from '@cdktf/provider-docker/lib/provider';
-import { Image } from '@cdktf/provider-docker/lib/image';
-import { Container } from '@cdktf/provider-docker/lib/container';
+import { DockerProvider } from "@cdktf/provider-docker/lib/provider";
+import { Image } from "@cdktf/provider-docker/lib/image";
+import { Container } from "@cdktf/provider-docker/lib/container";
 
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
 
-    // define resources here
-    new DockerProvider(this, 'docker', {});
+    new DockerProvider(this, "docker", {});
 
-    const dockerImage = new Image(this, 'nginxImage', {
-      name: 'nginx:latest',
+    const dockerImage = new Image(this, "nginxImage", {
+      name: "nginx:latest",
       keepLocally: false,
     });
 
-    new Container(this, 'nginxContainer', {
-      name: 'tutorial',
+    new Container(this, "nginxContainer", {
+      name: "tutorial",
       image: dockerImage.name,
       ports: [
-        { 
+        {
           internal: 80,
           external: 8000,
         },
@@ -30,5 +29,5 @@ class MyStack extends TerraformStack {
 }
 
 const app = new App();
-new MyStack(app, "typescript-docker");
+new MyStack(app, "learn-cdktf-docker");
 app.synth();
